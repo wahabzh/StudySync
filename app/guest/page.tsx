1d54247d4afd4dd73c470e96133cfe2c36408128
usjav <97538462+usjav@users.noreply.github.com>
2025-02-01 22:33:34 +0500

    Update page.tsx

--- app/guest/page.tsx
+++ app/guest/page.tsx
@@ -57,7 +57,7 @@ export default function HomePage() {
   return (
     (userId && <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
       <div className="flex items-center justify-between">
-        <h1 className="font-semibold text-lg md:text-2xl">My Documents</h1>
+        <h1 className="font-semibold text-lg md:text-2xl">Community Documents</h1>
       </div>
       <DocumentFilters setDocuments={setDocuments} userId={userId} />
       <div className="flex flex-col gap-4">
